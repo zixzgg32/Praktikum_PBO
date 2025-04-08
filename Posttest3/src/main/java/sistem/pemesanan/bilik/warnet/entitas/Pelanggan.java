1e62e53bff9e325fedca7239f8f5321c4e39bc7d
Zhorif Fachdiat <43207793+zixzgg32@users.noreply.github.com>
2025-04-08 21:48:14 +0700

    Add files via upload

---
+++ Posttest3/src/main/java/sistem/pemesanan/bilik/warnet/entitas/Pelanggan.java
@@ -0,0 +1,57 @@
+package sistem.pemesanan.bilik.warnet.entitas;
+
+public class Pelanggan {
+    private int idPelanggan;
+    private String nama;
+    private String email;
+    private String password;
+    private String noHp;
+
+    public Pelanggan(int idPelanggan, String nama, String email, String password, String noHp) {
+        this.idPelanggan = idPelanggan;
+        this.nama = nama;
+        this.email = email;
+        this.password = password;
+        this.noHp = noHp;
+    }
+
+    public int getIdPelanggan() {
+        return idPelanggan;
+    }
+
+    public void setIdPelanggan(int idPelanggan) {
+        this.idPelanggan = idPelanggan;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNoHp() {
+        return noHp;
+    }
+
+    public void setNoHp(String noHp) {
+        this.noHp = noHp;
+    }
+}
